b09b0abbbef9bac75e8899d3613b436fcecf19d9
Nguyen Thuc Tung <thuctung19029@gmail.com>
2019-04-15 19:31:23 +0700

    update and create file

--- zing/header.js
+++ zing/header.js
@@ -9,7 +9,9 @@ var search = document.getElementById('search');
 var userIcon = document.getElementById('userIcon');
 var user = document.getElementById('user');
 var headerBot = document.getElementById('header_bottom');
-var listMenu = document.getElementsByClassName('listMenu');
+var fscog = document.getElementById('fa-cog');
+var sidebar = document.getElementsByClassName('sidebar');
+var body = document.getElementById('body');
 
 // hiện ẩn chức năng bật tắt đèn
 tunr.onclick = function () {
@@ -33,16 +35,26 @@ cbk.onclick = function(e){
 		search.style.boxShadow = ".4px .4px 3px #5f5757";
 		userIcon.style.color = "#535863";
 		user.style.color = "#535863";
-		headerBot.style.background = "rgb(249, 246, 246)";
+		headerBot.style.background = "rgb(83, 141, 158)";
+		sidebar[0].style.background = "#e6e3e3";
+		sidebar[1].style.background = "#e6e3e3";
+		bat.style.background="#e6e3e3";
+		fscog.style.color = "black";
+		body.style.background = "#e6e3e3";
 		
 	}
 	else{
-		headerTop.style.background ="#171010e3";
-		image_zing.style.backgroundColor ="#2f2a2ae3";
+		headerTop.style.background ="black";
+		image_zing.style.backgroundColor ="black";
 		search.style.backgroundColor = "#131010";
 		userIcon.style.color = "#d7dbe6";
 		user.style.color = "#d7dbe6";
-		headerBot.style.background = "#292121";
+		headerBot.style.background = "#131010";
+		sidebar[0].style.background = "#292424";
+		sidebar[1].style.background = "#292424";
+		bat.style.background="#443e3e";
+		fscog.style.color = "grey";
+		body.style.background = "#292424";
 	}
 }
 
